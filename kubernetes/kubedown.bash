889db66942d9dd1ea5ac3033f0a7568412860747
kristijan.mirceta <kristijan.mirceta@gmail.com>
2017-12-13 01:06:29 +0100

    Added lots of kubeconfigs

---
+++ kubernetes/kubedown.bash
@@ -0,0 +1,8 @@
+kubectl delete deployments/accountoptions-deployment
+kubectl delete deployments/postgres-accountoptions-deployment
+kubectl delete services/accountoptions
+kubectl delete services/postgres-accountoptions
+kubectl delete deployments/users-deployment
+kubectl delete deployments/postgres-users-deployment
+kubectl delete services/users
+kubectl delete services/postgres-users
